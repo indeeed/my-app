a9c884d859ea136ac11c822504b0f0b9409e47bd
israel mailman <80362556+likefools@users.noreply.github.com>
2021-11-16 02:58:31 +0200

    לימודים חדשים

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -8,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
   title: string = 'game';
   titleText: string = 'משחק';
-  constructor() { }
+  
 
-  lists = ['1', '2', '3', '4', '5'];
-
-  ngOnInit(): void {
+  listsTo: number[]  = [];
+  constructor() { 
+    
   }
 
+  ngOnInit(): void { 
+    
+    for (let i = 0; i<8; i++) {
+      this.listsTo[i] = i + 1;
+    }
+    function writethis(){
+      alert('yes');
+    }
+  }
+
+  // אני רוצה לקבל את השם הראשון ברשימה
+   
 }
